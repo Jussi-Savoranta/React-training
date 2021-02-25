03ae952a6ab209a2f475c208a83584ddbe380663
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-02-25 10:02:07 +0200

    splitting app to components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import classes from './App.css';
-import Person from './Person/Person';
 
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit'
 
 
 class App extends Component {
@@ -20,7 +21,7 @@ class App extends Component {
     //const persons = this.state.persons.slice(); // slice will create a copy of a list and stores it to the variable
     const persons = [...this.state.persons]; // same thing but using spread (...) operator
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   nameChangeHandler = (event, id) => {
@@ -39,7 +40,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -50,48 +51,28 @@ class App extends Component {
   render() {
 
     let persons = null;
-    let btnClass = '';
+
 
     if (this.state.showPersons) {
-      persons = (
-        <div >
-          {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name} 
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangeHandler(event, person.id)}/>
-          })}
-
-        </div>
-      );
-          btnClass = classes.Red;
+      persons = <Persons
+        persons={this.state.persons}
+        clicked={this.deletePersonHandler}
+        changed={this.nameChangeHandler} />;
     }
     // one way of styling individual sections
     //let classes = ['red', 'bold'].join(' ');
 
-    let assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
-
     return (
       // two ways of making the click-functionality!!
-    
+
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working?</p>
-        <button
-        className={btnClass}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-          </button>
-          {persons}
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons} 
+          clicked={this.togglePersonsHandler}/>
+        {persons}
       </div>
-  
+
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
   }
