44218349e6a0fe6fd7b2b5d1858076d69e0e7e33
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-03-03 08:18:01 +0200

    useEffect to cockpit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        // you can use this like componentDidMount+componentDidUpdate in one method
+        // for http request and other stuff
+    })
 
     const assignedClasses = [];
     let btnClass = '';
