4c66a77d270ff424ebde38d0bbba77d6dc3ae8f3
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-03-04 11:06:50 +0200

    another form of hoc

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
 
 
 class App extends Component {
@@ -96,7 +97,7 @@ class App extends Component {
     return (
       // two ways of making the click-functionality!!
 
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showCockpit: false})}}>Remove cockpit</button>
         {this.state.showCockpit ? <Cockpit 
           title={this.props.appTitle}
@@ -105,11 +106,11 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
           /> : null}
         {persons}
-      </WithClass>
+      </Aux>
 
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
