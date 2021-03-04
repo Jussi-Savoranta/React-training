e246322b6dea315e6dbc6e43eb30bd0b6ddf94c4
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-03-04 09:35:10 +0200

    Aux as a wrapper

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            // <div className="Person" style={style}>
-            <div className={classes.Person}>
+            <Aux>
                 < p onClick={this.props.click} > I'm a {this.props.name} and I am {this.props.age} years old! {this.props.children}</p>
                 < p > {this.props.children}</p >
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
+            
         );
     }
 }
