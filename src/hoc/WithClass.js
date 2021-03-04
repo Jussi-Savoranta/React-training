0c6973e94ca090ef2294e74569abc997232fd314
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-03-04 12:13:33 +0200

    adding hoc component to person.js

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -3,9 +3,9 @@ import React from 'react';
 const withClass = (WrappedComponent, className) => {
     return props => (
         <div className={className}>
-            <WrappedComponent />
+            <WrappedComponent {...props}/>
         </div>
-    );
+    ); // WrappedComponent takes in multiple props so it's needed to spread through that single props variable
 };
 
 export default withClass;
